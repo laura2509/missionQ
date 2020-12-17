59c37ebb9e9f01123369ad21299005738c395e72
laura2509 <laura.blondy@gmail.com>
2020-12-17 21:42:00 +0100

    Changes in custom use displayName hook.

--- src/hooks/useDisplayName.tsx
+++ src/hooks/useDisplayName.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
 
 export const useDisplayName = (propsMessage: string | undefined, displayName: string | undefined) => {
-    const [name, setName] = useState(displayName);
 
     useEffect(() => {
         console.log(`${propsMessage} ${displayName}`);
